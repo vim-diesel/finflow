022b4ce028ae9ac3df816a1c7ddc7dffa5239ba8
Ian C <ian.clark@hotmail.com>
2024-10-20 20:08:19 -0400

    fix dark mode text in burger menu in hero

--- app/hero.tsx
+++ app/hero.tsx
@@ -93,7 +93,7 @@ export default function Hero() {
                     <a
                       key={item.name}
                       href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-200/20"
                     >
                       {item.name}
                     </a>
@@ -102,7 +102,7 @@ export default function Hero() {
                 <div className="py-6">
                   <Link
                     href="/login"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-gray-100 dark:hover:bg-gray-200/20"
                   >
                     Log in
                   </Link>
