d19cb7e80b08e4526f4d965e0b88e639aec904ae
Ian C <ian.clark@hotmail.com>
2024-10-26 19:48:22 -0400

    dark mode enabled

--- components/tailwindui/budgetTable.tsx
+++ components/tailwindui/budgetTable.tsx
@@ -19,7 +19,7 @@ export default function BudgetTable({
           <h1 className="text-base font-semibold leading-6 text-gray-900">
             Categories
           </h1>
-          <p className="mt-2 text-sm text-gray-700">
+          <p className="mt-2 text-sm text-gray-700 dark:text-gray-300">
             A list of all the categories in your budget
           </p>
         </div>
@@ -36,29 +36,29 @@ export default function BudgetTable({
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
             <table className="min-w-full">
-              <thead className="bg-white">
+              <thead className="bg-white dark:bg-black">
                 <tr>
                   <th
                     scope="col"
-                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3 dark:text-gray-200"
                   >
                     Category
                   </th>
                   <th
                     scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 dark:text-gray-200"
                   >
                     Assigned
                   </th>
                   <th
                     scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 dark:text-gray-200"
                   >
                     Activity
                   </th>
                   <th
                     scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 dark:text-gray-200"
                   >
                     Available
                   </th>
@@ -67,20 +67,20 @@ export default function BudgetTable({
                   </th>
                 </tr>
               </thead>
-              <tbody className="bg-white">
-                {categoryGroups.map((categoryGroup) => (
+              <tbody className="bg-white dark:bg-gray-600">
+                {categoryGroups?.map((categoryGroup) => (
                   <Fragment key={categoryGroup.id}>
-                    <tr className="border-t border-gray-200">
+                    <tr className="border-t border-gray-200 ">
                       <th
                         scope="colgroup"
                         colSpan={5}
-                        className="bg-gray-50 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
+                        className="bg-gray-50 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3 dark:bg-gray-500 dark:text-gray-200"
                       >
                         {categoryGroup.name}
                       </th>
                     </tr>
                     {categories
-                      .filter((category) => category.id === categoryGroup.id)
+                      ?.filter((category) => category.id === categoryGroup.id)
                       .map((category, categoryIdx) => (
                         <tr
                           key={category.id}
@@ -91,25 +91,32 @@ export default function BudgetTable({
                             "border-t",
                           )}
                         >
-                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3 dark:text-gray-200">
                             {category.name}
                           </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            ${category.amount_assigned ?? 0}
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                            ${category.amount_spent ?? 0}
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 dark:text-gray-300">
                             $
-                            {(category.amount_assigned ?? 0) -
-                              (category.amount_spent ?? 0) +
-                              (category.carryover_from_previous_month ?? 0)}
+                            {category.monthly_category_details
+                              ?.amount_assigned ?? 0}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 dark:text-gray-300">
+                            $
+                            {category.monthly_category_details?.amount_spent ??
+                              0}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 dark:text-gray-300">
+                            $
+                            {(category.monthly_category_details
+                              ?.amount_assigned ?? 0) -
+                              (category.monthly_category_details
+                                ?.amount_spent ?? 0) +
+                              (category.monthly_category_details
+                                ?.carryover_from_previous_month ?? 0)}
                           </td>
                           <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
                             <a
                               href="#"
-                              className="text-indigo-600 hover:text-indigo-900"
+                              className="text-indigo-600 hover:text-indigo-900 dark:text-indigo-300 dark:hover:text-indigo-400"
                             >
                               Edit
                               <span className="sr-only">, {category.name}</span>
